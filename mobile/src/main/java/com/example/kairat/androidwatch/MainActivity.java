af4865406dbae68d46b7e54f16084245414eb58c
Wei Low <weiwlow@gmail.com>
2015-04-19 18:09:51 -0400

    Buttons placed for main activity java

--- mobile/src/main/java/com/example/kairat/androidwatch/MainActivity.java
+++ mobile/src/main/java/com/example/kairat/androidwatch/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.kairat.androidwatch;
 
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 import java.util.Random;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.view.ViewStub;
 
 /*
 * JSON Format for JSONProcess Class "time:activity:budgetMax"
@@ -24,12 +31,42 @@ public class MainActivity extends ActionBarActivity {
     private static int random_code= 6;
     private String myString;
 
+    private TextView mTextView;
+    private int startHour;
+    private int startMinute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void setStartTime(int hourOfDay, int minute) {
+        startHour = hourOfDay;
+        startMinute = minute;
+    }
+
+    public void clickTime(View view) {
+        // TODO Auto-generated method stub
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                setStartTime(selectedHour,selectedMinute);
+                //System.out.println(startHour + " " + startMinute);
+                //DEBUG statement above: Basically prints to console the time in 24 hour format
+            }
+        }, hour, minute, false);//No to 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+    }
+
+    public void getLocation(View view) {
+        //get GPS coordinates
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
