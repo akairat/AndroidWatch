4fbb793e0aa9eab9fd82fe9ed8ad935e7165fe0b
Wei Low <weiwlow@gmail.com>
2015-04-19 15:40:45 -0400

    Added the two processes that can be invoked via the Main activity (JSONProcess & helpMenu) Created prelim layout for the main menu screen **DOES NOT COMPILE**

--- mobile/src/main/java/com/example/kairat/androidwatch/MainActivity.java
+++ mobile/src/main/java/com/example/kairat/androidwatch/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.kairat.androidwatch;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import java.util.Random;
+import android.widget.EditText;
 
+/*
+* JSON Format for JSONProcess Class "time:activity:budgetMax"
+* TODO: Decide on 4-6 Activities to have
+* TODO: Decide on what the Watch can send to phone
+* TODO: Add a general menu to the help menu class to display
+* TODO: Fill in JSONProcess class
+* */
 
 public class MainActivity extends ActionBarActivity {
+
+    private static int help_code = 3;
+    private static int adventure_code= 5;
+    private static int random_code= 6;
+    private String myString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,37 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void resetAll(View view) {
+        //reset all checkboxes
+    }
+    //Starts an that gives general directions on application use
+    public void helpMenu(View view) {
+        Intent hm = new Intent(this, helpMenu.class);
+        startActivityForResult(hm, help_code);
+    }
+
+    //Uses a random number generator to pick an activity to do
+    public void randomAct(View view) {
+        Random rg = new Random();
+        int ri = rg.nextInt(6);
+        switch (ri) {
+            case 0:
+                //myString = "time:activity:activity:budgetMin:budgetMax"
+        }
+        Intent ra = new Intent(this, JSONProcess.class);
+        startActivityForResult(ra, random_code);
+    }
+
+    //Uses the user's selections to start the next activity
+    //With regards to the watch: time picker + voice activation will be used to pick items
+        //pressing "go" on the watch will pass in appropriate JSON info the JSONProcess class
+    public void startNext(View view) {
+        Intent i = new Intent(this, JSONProcess.class);
+        myString = "these:words:are:separated:by:colons";
+        i.putExtra("qString", myString);
+        startActivityForResult(i, adventure_code);
     }
 
     @Override
