1aec628b3b72a2d7978a52728cb27a8d2d9112b2
akairat <kairat.ashim@gmail.com>
2015-04-21 18:25:08 -0400

    Merge branch 'master' of https://github.com/akairat/AndroidWatch
    Conflicts:
    	mobile/src/main/AndroidManifest.xml
    	mobile/src/main/res/layout/activity_main.xml
    	mobile/src/main/res/values/strings.xml
    
    Trying to implement watch messaging

--- mobile/src/main/java/com/example/kairat/androidwatch/ListenerService.java
+++ mobile/src/main/java/com/example/kairat/androidwatch/ListenerService.java
@@ -1,7 +1,26 @@
 package com.example.kairat.androidwatch;
 
-/**
- * Created by kairat on 4/21/15.
- */
-public class ListenerService {
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class ListenerService extends WearableListenerService {
+
+    private static final String TAG = "MOBILE MESSAGE: ";
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.d(TAG, "Message Received");
+        Log.d(TAG, messageEvent.getPath());
+        showToast(messageEvent.getPath());
+    }
+
+    private void showToast(String message) {
+        Log.d(TAG, "showToast is called");
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Log.d(TAG, "toast was shown");
+    }
 }
--- wear/src/main/java/com/example/kairat/androidwatch/ManualActivity.java
+++ wear/src/main/java/com/example/kairat/androidwatch/ManualActivity.java
@@ -2,31 +2,137 @@ package com.example.kairat.androidwatch;
 
 import android.app.Activity;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-public class ManualActivity extends Activity implements TimePickerFragment.TimePickerFragmentListener {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ManualActivity extends Activity /*implements ChoicesFragment.ChoicesFragmentListener*/ {
 
     private TextView mTextView;
     private int startHour;
     private int startMinute;
 
+    private static final long CONNECTION_TIME_OUT_MS = 100;
+    private static String MESSAGE = "#";
+
+    private static final String TAG = "KAIRATS MESSAGE";
+    private GoogleApiClient client;
+    private String nodeId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+
+        initApi();
+
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                // showChoiceButtons();
             }
         });
 
-        showTimePickerDialog();
+        //showTimePickerDialog();
+        //showChoiceFragment();
     }
 
+    /**
+     * Initializes the GoogleApiClient and gets the Node ID of the connected device.
+     */
+    private void initApi() {
+        client = getGoogleApiClient(this);
+        retrieveDeviceNode();
+    }
+
+    /**
+     * Returns a GoogleApiClient that can access the Wear API.
+     * @param context
+     * @return A GoogleApiClient that can make calls to the Wear API
+     */
+    private GoogleApiClient getGoogleApiClient(Context context) {
+        return new GoogleApiClient.Builder(context)
+                .addApi(Wearable.API)
+                .build();
+    }
+
+    /**
+     * Connects to the GoogleApiClient and retrieves the connected device's Node ID. If there are
+     * multiple connected devices, the first Node ID is returned.
+     */
+    private void retrieveDeviceNode() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                NodeApi.GetConnectedNodesResult result =
+                        Wearable.NodeApi.getConnectedNodes(client).await();
+                List<Node> nodes = result.getNodes();
+                if (nodes.size() > 0) {
+                    nodeId = nodes.get(0).getId();
+                }
+                client.disconnect();
+            }
+        }).start();
+    }
+
+
+    /**
+     * Sends a message to the connected mobile device, telling it to show a Toast.
+     */
+    /*
+    @Override
+    public void sendChoice(String choice) {
+        MESSAGE = choice;
+        Log.d(TAG, "SENDING CHOICE");
+        if (nodeId != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                    Wearable.MessageApi.sendMessage(client, nodeId, MESSAGE, null).setResultCallback(
+                            new ResultCallback<MessageApi.SendMessageResult>() {
+                                @Override
+                                public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                                    if (!sendMessageResult.getStatus().isSuccess()){
+                                        Log.e(TAG, "Failed to send message with status code: "
+                                                + sendMessageResult.getStatus().getStatusCode());
+                                    } else {
+                                        Log.i(TAG, "message was sent: "+sendMessageResult.getStatus().getStatusCode());
+                                    }
+                                }
+                            }
+                    );
+                }
+            });
+        }
+    }
+
+
+    public void showChoiceFragment() {
+        ChoicesFragment cf = new ChoicesFragment();
+    }
+    */
+
+
+/*
+TimePicker Dialog
     @Override
     public void setStartTime(int hourOfDay, int minute) {
         startHour = hourOfDay;
@@ -37,4 +143,50 @@ public class ManualActivity extends Activity implements TimePickerFragment.TimeP
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getFragmentManager(), "timePicker");
     }
+
+    */
+
+    public void chooseFood(View v){
+        sendChoice("food");
+    }
+
+    public void chooseMuseum(View v){
+        sendChoice("museum");
+    }
+
+    public void choosePark(View v) {
+        sendChoice("park");
+    }
+
+    public void chooseShopping(View v){
+        sendChoice("shopping");
+    }
+
+    public void sendChoice(String choice) {
+        MESSAGE = choice;
+        Log.d(TAG, nodeId);
+        if (nodeId != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "SENDING CHOICE"+MESSAGE);
+                    client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+                    Wearable.MessageApi.sendMessage(client, nodeId, MESSAGE, null).setResultCallback(
+                            new ResultCallback<MessageApi.SendMessageResult>() {
+                                @Override
+                                public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                                    if (!sendMessageResult.getStatus().isSuccess()){
+                                        Log.e(TAG, "Failed to send message with status code: "
+                                                + sendMessageResult.getStatus().getStatusCode());
+                                    } else {
+                                        Log.i(TAG, "message was sent: "+sendMessageResult.getStatus().getStatusCode());
+                                    }
+                                }
+                            }
+                    );
+                }
+            });
+        }
+    }
+
 }
