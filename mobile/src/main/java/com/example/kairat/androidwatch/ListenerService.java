0ce7d689da7e1cbf5bab9e613e7e29d1e5fc724b
Wei Low <weiwlow@gmail.com>
2015-04-27 14:00:17 -0400

    Watch ListenerService

--- mobile/src/main/java/com/example/kairat/androidwatch/ListenerService.java
+++ mobile/src/main/java/com/example/kairat/androidwatch/ListenerService.java
@@ -3,9 +3,12 @@ package com.example.kairat.androidwatch;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
@@ -26,135 +29,102 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
-public class ListenerService extends WearableListenerService implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListenerService extends WearableListenerService implements DownloadResultReceiver.Receiver {
 
     private boolean mIsInResolution;
     protected static final int REQUEST_CODE_RESOLUTION = 1;
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
-    private int startHour;
-    private int startMinute;
+
     private String pickActivity;
     private double latitude;
     private double longitude;
 
-    private Location mLastLocation;
-    private Location mCurrentLocation;
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-
     private static final String TAG = "MOBILE MESSAGE: ";
     String PlaceType;
 
+    int i =0;
+    private String LOG_MESSAGE = "WebAPIExample";
 
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+    List<String> suggested_place_name;
+    List<String> suggested_place_address;
+    List<String> suggested_place_geo;
+    List<String> suggested_place_distance;
+    List<String> suggested_place_duration;
+
+    JSONArray place_details = null;
+    JSONArray place_list= null;
+
+    private String PlaceLocation;
+
+    private DownloadResultReceiver mReceiver;
+    public void call_intent(){
+        /* Starting Download Service */
+        mReceiver = new DownloadResultReceiver(new Handler());
+        mReceiver.setReceiver(this);
+        Intent intent = new Intent(Intent.ACTION_SYNC, null, this, GetResultService.class);
+
+/* Send optional extras to Download IntentService */
+        intent.putExtra("PlaceType",PlaceType);
+        intent.putExtra("PlaceLocation", PlaceLocation);
+
+        intent.putExtra("receiver", mReceiver);
+        intent.putExtra("requestId", 101);
+
+        startService(intent);
     }
 
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil
-                .isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                System.out.println(resultCode);
-            } else {
-                Toast.makeText(getApplicationContext(),
-                        "This device is not supported.", Toast.LENGTH_LONG)
-                        .show();
-            }
-            return false;
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        switch (resultCode) {
+            case GetResultService.STATUS_RUNNING:
+
+                break;
+            case GetResultService.STATUS_FINISHED:
+                /* Hide progress & extract result from bundle */
+
+                String[] place_name = resultData.getStringArray("place_name");
+                String[] place_address = resultData.getStringArray("place_address");
+                String[] place_geo = resultData.getStringArray("place_geo");
+                String[] place_distance = resultData.getStringArray("place_distance");
+                String[] place_duration = resultData.getStringArray("place_duration");
+                Log.i(LOG_MESSAGE, " PlacesInfo" + place_name);
+                Log.i(LOG_MESSAGE, " PlacesInfo" + place_address);
+                Log.i(LOG_MESSAGE, " PlacesInfo" + place_geo);
+                Log.i(LOG_MESSAGE, " PlacesInfo" + place_distance);
+                Log.i(LOG_MESSAGE, " PlacesInfo" + place_duration);
+                suggested_place_name = Arrays.asList(place_name);
+                suggested_place_address= Arrays.asList(place_address);
+                suggested_place_geo = Arrays.asList(place_geo);
+                suggested_place_distance = Arrays.asList(place_distance);
+                suggested_place_duration = Arrays.asList(place_duration);
+
+
+                break;
+            case GetResultService.STATUS_ERROR:
+                /* Handle the error */
+                String error = resultData.getString(Intent.EXTRA_TEXT);
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+                break;
         }
-        return true;
     }
 
-    public void getLocation() {
-        // Get the location manager
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            latitude = mLastLocation.getLatitude();
-            longitude = mLastLocation.getLongitude();
-        }
-        else {
-            System.out.println("Location was not obtained!!!!");
-        }
-        System.out.println(latitude + ",,,,,," + longitude);
-        Context context = getApplicationContext();
-        CharSequence text = "!!!!Location Set to: "+ latitude + "," + longitude;
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.d(TAG, "Message Received");
         PlaceType = messageEvent.getPath();
-        if (checkPlayServices()) {
-            buildGoogleApiClient();
-            getLocation();
-        }
+        PlaceLocation = "42.3613154,-71.0912821";
 
+        call_intent();
     }
 
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        //Using GooglePlay Services Location API
-        mLastLocation= LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            latitude = mLastLocation.getLatitude();
-            longitude = mLastLocation.getLongitude();
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-        latitude = mCurrentLocation.getLatitude();
-        longitude = mCurrentLocation.getLongitude();
-    }
-
-    /**
-     * Called when {@code mGoogleApiClient} connection is suspended.
-     */
-    @Override
-    public void onConnectionSuspended(int cause) {
-        System.out.println("GoogleApiClient connection suspended");
-        retryConnecting();
-    }
-
-    /**
-     * Called when {@code mGoogleApiClient} is trying to connect but failed.
-     * Handle {@code result.getResolution()} if there is a resolution
-     * available.
-     */
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        System.out.println("GoogleApiClient connection failed: " + result.toString());
-        if (!result.hasResolution()) {
-            // Show a localized error dialog.
-            System.out.println("Error in connection");
-            return;
-        }
-        // If there is an existing resolution error being displayed or a resolution
-        // activity has started before, do nothing and wait for resolution
-        // progress to be completed.
-        if (mIsInResolution) {
-            return;
-        }
-        mIsInResolution = true;
-        retryConnecting();
-    }
-
-    private void retryConnecting() {
-        mIsInResolution = false;
-        if (!mGoogleApiClient.isConnecting()) {
-            mGoogleApiClient.connect();
-        }
-    }
 }
